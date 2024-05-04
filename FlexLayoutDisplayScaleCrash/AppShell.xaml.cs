4e8856a6886ceb66487328f462910913709836fe
Ronan Burke <burkuscat@gmail.com>
2024-05-04 14:31:50 +0100

    New MAUI project

---
+++ FlexLayoutDisplayScaleCrash/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexLayoutDisplayScaleCrash
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FlexLayoutDisplayScaleCrash/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlexLayoutDisplayScaleCrash
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
